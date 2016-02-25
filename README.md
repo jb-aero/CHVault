519454897eaed2bca96defa804dbf1b9121f50cf
jb_aero <jb_aero@zeoldcraft.com>
2016-02-25 00:02:22 -0500

    Add Chat formatting class of functions.

--- README.md
+++ README.md
@@ -11,13 +11,27 @@ Download the correct version for your version of CommandHelper:
 ### Permissions
 Uses Vault to extend the potential of permissions checking with group and multiworld support.
 
-#### boolean vault_has_permission(player, permission, [world])
+#### boolean vault_has_permission(player, permission, [world]);
 Checks the permission value of a player, optionally in a specific world.
 When used on an offline player, the accuracy depends on the permission plugin.
 
-#### array vault_pgroup(player, [world])
+#### array vault_pgroup(player, [world]);
 Returns an array of groups that the player is in. When used on offline players,
 the accuracy of this function is dependent on the permissions plugin.
+
+### Chat
+
+#### string vault_group_prefix(world, group);
+Returns the prefix of group in world.
+
+#### string vault_group_suffix(world, group);
+Returns the suffix of group in world.
+
+#### string vault_user_prefix(world, user);
+Returns the prefix of user in world. Accuracy is plugin dependent if user is not online.
+
+#### string vault_user_suffix(world, user);
+Returns the suffix of user in world. Accuracy is plugin dependent if user is not online.
 
 ### Economy
 Provides functions to hook into the server's economy plugin. To use any of these functions, you must have one of the following economy plugins installed: iConomy 4,5,6, BOSEconomy 6 & 7, EssentialsEcon, 3Co, MultiCurrency, MineConomy, eWallet, EconXP, CurrencyCore, CraftConomy. In addition, you must download the [Vault plugin](http://dev.bukkit.org/server-mods/vault/). Beyond this, there is no special setup to get the economy functions working, assuming they work for you in game using the plugin's default controls. Bank controls may not be supported in your particular plugin, check the details of that particular plugin.
---
+++ src/main/java/io/github/jbaero/chvault/functions/ChatFormatting.java
@@ -0,0 +1,117 @@
+package io.github.jbaero.chvault.functions;
+
+import com.laytonsmith.annotations.api;
+import com.laytonsmith.core.Static;
+import com.laytonsmith.core.constructs.CString;
+import com.laytonsmith.core.constructs.Construct;
+import com.laytonsmith.core.constructs.Target;
+import com.laytonsmith.core.environments.Environment;
+import com.laytonsmith.core.exceptions.CRE.CREPluginInternalException;
+import com.laytonsmith.core.exceptions.ConfigRuntimeException;
+import io.github.jbaero.chvault.CHVault.jFunction;
+import net.milkbowl.vault.chat.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+/**
+ * Chat, 2/24/2016 9:31 PM
+ *
+ * @author jb_aero
+ */
+public class ChatFormatting {
+
+	static Chat getChat(Target t) throws CREPluginInternalException {
+
+		try {
+			return Bukkit.getServer().getServicesManager().getRegistration(Chat.class).getProvider();
+		} catch (NullPointerException npe) {
+			throw new CREPluginInternalException("Could not connect to chat features of Vault.", t);
+		}
+	}
+
+	@api
+	public static class vault_group_prefix extends jFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			Static.checkPlugin("Vault", t);
+			return new CString(getChat(t).getGroupPrefix(args[0].val(), args[1].val()), t);
+		}
+
+		public String getName() {
+			return "vault_group_prefix";
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "string {world, group} Returns the prefix of group in world.";
+		}
+	}
+
+	@api
+	public static class vault_group_suffix extends jFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			Static.checkPlugin("Vault", t);
+			return new CString(getChat(t).getGroupSuffix(args[0].val(), args[1].val()), t);
+		}
+
+		public String getName() {
+			return "vault_group_suffix";
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "string {world, group} Returns the suffix of group in world.";
+		}
+	}
+
+	@api
+	public static class vault_user_prefix extends jFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			Static.checkPlugin("Vault", t);
+			return new CString(getChat(t).getPlayerPrefix(args[0].val(),
+					(OfflinePlayer) Static.GetUser(args[1], t).getHandle()), t);
+		}
+
+		public String getName() {
+			return "vault_user_prefix";
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "string {world, user} Returns the prefix of user in world. Accuracy is plugin dependent.";
+		}
+	}
+
+	@api
+	public static class vault_user_suffix extends jFunction {
+
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			Static.checkPlugin("Vault", t);
+			return new CString(getChat(t).getPlayerSuffix(args[0].val(),
+					(OfflinePlayer) Static.GetUser(args[1], t).getHandle()), t);
+		}
+
+		public String getName() {
+			return "vault_user_suffix";
+		}
+
+		public Integer[] numArgs() {
+			return new Integer[]{2};
+		}
+
+		public String docs() {
+			return "string {world, user} Returns the suffix of user in world. Accuracy is plugin dependent.";
+		}
+	}
+}
